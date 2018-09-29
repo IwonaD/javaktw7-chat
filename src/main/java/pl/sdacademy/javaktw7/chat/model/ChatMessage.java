cd34694df2c6bad83856a6ad02bdd06353894283
IwonaD <Iwonadanel1>
2018-09-29 10:22:14 +0200

    chat - beginning of chat project

---
+++ src/main/java/pl/sdacademy/javaktw7/chat/model/ChatMessage.java
@@ -0,0 +1,31 @@
+package pl.sdacademy.javaktw7.chat.model;
+
+import java.io.Serializable;
+
+public class ChatMessage implements Serializable {
+
+    //zaimplementowac interfej Serializable
+    //wymusic wersje klasy na numer 1L
+
+    public static final long serialVersionUID = 1L;
+
+    private String author;
+    private String message;
+
+    public ChatMessage(){
+
+    }
+
+    public ChatMessage(String author, String message) {
+        this.author = author;
+        this.message = message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
